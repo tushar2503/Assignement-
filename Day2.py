1948226bf5f9b9aac6c27aabe3cd1ad34d09e9e3
tushar2503 <98078852+tushar2503@users.noreply.github.com>
2022-01-20 13:28:03 +0530

    Day 2 Assignment Submitted

---
+++ Day2.py
@@ -0,0 +1,20 @@
+a = "tushar"
+print(type(a))
+
+b = 84
+print(type(b))
+
+c = 65.47
+print(type(c))
+
+d = True
+print(type(d))
+
+e = [4, 3, 2, 4]
+print(type(e))
+
+f = {2, 4, 5, 3, 4}
+print(type(f))
+
+g = (4, 2, 5, 2, 2)
+print(type(g))
