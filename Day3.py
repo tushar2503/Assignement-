1264ea2d7da631b071b57baac8e943677013c65a
tushar2503 <98078852+tushar2503@users.noreply.github.com>
2022-01-20 13:35:20 +0530

    Day 3

---
+++ Day3.py
@@ -0,0 +1,16 @@
+a = ['Tushar']
+name = input("Enter for Name:" )
+print(input(a))
+
+b = [18]
+age = input("Enter for age:" )
+print(input(b))
+
+c = ['TCSC']
+clg = input('Enter for clg:' )
+print(input(c))
+
+d = ['Higher secondary']
+qualification = input("Enter for qualification:" )
+print(input(d))
+
