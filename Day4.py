d0c135dcdad33f7a6e34bb70146d96c97fe667e9
tushar2503 <98078852+tushar2503@users.noreply.github.com>
2022-01-21 13:30:08 +0530

    Add files via upload

---
+++ Day4.py
@@ -0,0 +1,40 @@
+#function 1
+
+a = "tushar"
+print(type(a))
+
+b = 84
+print(type(b))
+
+c = 65.47
+print(type(c))
+
+d = True
+print(type(d))
+
+e = [4, 3, 2, 4]
+print(type(e))
+
+f = {2, 4, 5, 3, 4}
+print(type(f))
+
+g = (4, 2, 5, 2, 2)
+print(type(g))
+
+
+#function 2
+
+a_1 = [2, 3, 2, 3]
+print(a_1)
+
+
+#function 3
+
+a = {
+    'name': 'tushar',
+    'age' : '18',
+    'std' : 'BSC Cs',
+    'country': 'india',
+    'location': 'mumbai',
+}
+print(a)
