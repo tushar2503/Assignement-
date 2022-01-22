1469b610e921c827af37dc47d112d3d02dd33ca2
tushar2503 <98078852+tushar2503@users.noreply.github.com>
2022-01-22 12:09:34 +0530

    Day5 Assignment

---
+++ Day5 assingment.py
@@ -0,0 +1,12 @@
+#django installed
+#running successfully
+
+print("hello world")
+import django 
+print(django.get_version())
+
+# type python manage.py runserver in terminal
+#ctrl+click on the provided link
+#click on the link and run it on your preferred server
+
+# http://127.0.0.1:8000/ for assingnment
